5de63abe512e74cc51dec218571b04c4c46d780b
Traipop <traipopwichiansarn@gmail.com>
2025-12-16 15:53:43 +0700

    lab8_3 completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,58 +1,42 @@
 #include<iostream>
-#include<iomanip>
-#include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
-char findGrade(double score){
-	if(score > 90){
-		return 'A';
-	} else if((score > 75) && (score <= 90)){
-		return 'B';
-	} else if((score > 60) && (score <= 75)){
-		return 'C';
-	} else if((score > 45) && (score <= 60)){
-		return 'D';
-	} else {
-		return 'F';
-	}
-}
 int main(){
-	//Input the number of students
-	int N,i = 0;
-	cout << "Enter the number of students: ";
-	cin >> N;
-	string name[N];
-	float score[N];	
-	
-	//Store names and scores of students into an array 
-	while(i < N){
-		cout << "Name of student " << i+1 << ": ";
-		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
-        getline(cin,name[i]);
-		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
-        cin >> score[i];
-		i++;
-	}
-	
-	//Print names scores and grades
-	i = 0;
-	cout << "---------------------------------------------\n";
-	cout << setw(25) << "Name" << setw(8) << "Score" << setw(8) << "Grade" << "\n";
-	cout << "---------------------------------------------\n";
-	while(i < N){
-		cout << setw(25) << name[i] << setw(8) << score[i] << setw(8) << findGrade(score[i]) << "\n";
-		i++;
-	} 
-	cout << "---------------------------------------------\n";
-
-	return 0;
+    long long int age ,height,bounty;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age <= 25){
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height < 100){
+            cout << "Your character = Chopper" ;
+        } else if(height < 180){
+            cout << "Your character = Usopp" ;
+        } else {
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if(bounty > 1100000000){
+               cout << "Your character = Zoro" ; 
+            } else {
+                cout << "Your character = Sanji" ;
+            }
+        }
+    } else if((age > 25)&&(age <= 60)){
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if(bounty > 500000000){
+            cout << "Your character = Jinbe" ; 
+        } else {
+            cout << "Your character = Franky" ;
+        }
+    } else {
+            cout << "Your character = Brook" ;
+        }
 }
+
 /*
-"Enter your age: "
-"Enter your height: "
+
+
 "Enter your bounty: "
 "Your character = "
 "Zoro"
