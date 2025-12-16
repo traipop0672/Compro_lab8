3a76f8ce8d123477ddea32eb02f3ddf7f557462b
Traipop <traipopwichiansarn@gmail.com>
2025-12-16 15:22:09 +0700

    lab8_2 completed

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -4,6 +4,19 @@
 using namespace std;
 
 //[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double score){
+	if(score > 90){
+		return 'A';
+	} else if((score > 75) && (score <= 90)){
+		return 'B';
+	} else if((score > 60) && (score <= 75)){
+		return 'C';
+	} else if((score > 45) && (score <= 60)){
+		return 'D';
+	} else {
+		return 'F';
+	}
+}
 
 int main(){
 	//Input the number of students
