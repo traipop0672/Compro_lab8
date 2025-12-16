7d22f87cf0106fcb97b1dbb669024f78002717f2
Traipop <traipopwichiansarn@gmail.com>
2025-12-16 15:29:43 +0700

    lab8_2 completed

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -17,7 +17,6 @@ char findGrade(double score){
 		return 'F';
 	}
 }
-
 int main(){
 	//Input the number of students
 	int N,i = 0;
@@ -31,8 +30,10 @@ int main(){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+        getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
 		//[Missing Code 3] Get score of the i-th students.
+        cin >> score[i];
 		i++;
 	}
 	
